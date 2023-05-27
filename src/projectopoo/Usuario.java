120869eeb16d48669b06d610f5baef1baa58e3e5
Migue <Migue@192.168.1.14>
2023-05-27 16:57:35 -0500

    mensaje 3

--- src/projectopoo/Usuario.java
+++ src/projectopoo/Usuario.java
@@ -9,5 +9,49 @@ package projectopoo;
  * @author deavi
  */
 public class Usuario {
-    
+        private String nombre;
+        private String apellido;
+        private int cedula;
+        private String usuario;
+        private String contraseña;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
